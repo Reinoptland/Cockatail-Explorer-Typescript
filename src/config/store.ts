0baaa25c75669e6d95861f3779e99ec042981fa1
reinoptland <reinoptland@gmail.com>
2019-08-30 10:48:25 +0200

    Configure redux store, add Cocktail interface

---
+++ src/config/store.ts
@@ -0,0 +1,19 @@
+import { compose,  createStore } from 'redux'
+import reducer from '../reducers'
+import { ICocktail } from '../entities/cocktail'
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+const enhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+
+const store = createStore(reducer, enhancer)
+
+export interface IStore {
+  cocktails: ICocktail[];
+}
+
+export default store
---
+++ src/reducers/cocktails.ts
@@ -0,0 +1,10 @@
+import { ICocktail } from '../entities/cocktail'
+
+const reducer = (state: ICocktail[] | [] = [], action: any = {}) => {
+    switch (action.type) {
+      default:
+        return state
+    }
+}
+  
+export default reducer;
